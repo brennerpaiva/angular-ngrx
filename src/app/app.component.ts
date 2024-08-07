78da56c80e5a1b4b8ab7df6ba3000f8a32b07554
Brenner Paiva <brennerpaiva@gmail.com>
2024-08-06 22:28:44 -0300

    teste de manipulação de estados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Livro } from './model/livro.model';
+import { LivroStateService } from './service/livro-state.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-rxjs';
+  protected livros$ = this.livroSateService.escutarMudancasdeLivros();
+  livroInput = ''
+
+  constructor(private livroSateService: LivroStateService){}
+
+  ngOnInit() {
+    this.livroSateService.carregarLivros();
+  }
+
+  adicionar() {
+    const livro: Livro = {
+      id: 10,
+      nome: this.livroInput
+    }
+
+    this.livroSateService.adicionarLivro(livro);
+  }
 }
